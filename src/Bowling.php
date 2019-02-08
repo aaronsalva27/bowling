9c5740fa294d735e1a72893ff0b64adfe00c5e1f
aaronsalva27 <aaronsalva27@gmail.com>
2019-02-08 11:43:59 +0100

    unit test

--- src/Bowling.php
+++ src/Bowling.php
@@ -2,4 +2,79 @@
 
 class Bowling
 {
+    public function getScore($line): int
+    {
+        $formattedLine = $this->formatLine($line);
+
+        if (count($formattedLine) < 12) {
+            throw new \InvalidArgumentException('Incorrect length');
+        }
+
+        return $this->calculateScore($formattedLine);
+    }
+
+
+    private function calculateScore($line): int
+    {
+        $score = 0;
+        $framesCount = 0;
+
+        for ($currentFrame = 0; $currentFrame < count($line) - 1;
+             $currentFrame += 2) {
+
+            $framesCount++;
+            $score += $line[$currentFrame];
+            $score += $line[$currentFrame + 1];
+
+            if ($this->isSpare($line, $currentFrame)) {
+                $score += $line[$currentFrame + 2];
+            }
+
+            if ($this->isStrike($line, $currentFrame) && isset($line[$currentFrame + 2])) {
+                $score += $line[$currentFrame + 2];
+                $currentFrame--;
+            }
+
+            if ($this->isLastFrame($framesCount)) {
+                break;
+            }
+        }
+
+        return $score;
+    }
+
+    private function isLastFrame($count)
+    {
+        return $count>= 10;
+    }
+
+    private function isStrike($line, $frameIndex): bool
+    {
+        return $line[$frameIndex] === 10;
+    }
+
+    private function isSpare($line, $frameIndex): bool
+    {
+        return $line[$frameIndex] + $line[$frameIndex + 1] === 10;
+    }
+
+    private function formatLine($line): array
+    {
+        $formattedLine = [];
+
+        foreach (explode(' ', $line) as $frame) {
+            if (!is_int(intval($frame))) {
+                throw new InvalidArgumentException('Incorrect argument');
+            }
+
+            if (intval($frame) > 10) {
+                throw new InvalidArgumentException('Incorrect argument');
+            }
+
+            array_push($formattedLine, intval($frame));
+        }
+
+        return $formattedLine;
+    }
+
 }
--- tests/BowlingTest.php
+++ tests/BowlingTest.php
@@ -8,4 +8,152 @@ class BowlingTest extends \PHPUnit\Framework\TestCase
     {
         $this->sut = new Bowling();
     }
+
+
+    /**
+     * @test
+     * @dataProvider getLineAllGuttersProvider
+     */
+    public function testAllGutterBallsGetScore($line)
+    {
+        $this->assertEquals($this->sut->getScore($line), 0);
+    }
+
+    /**
+     * @test
+     * @dataProvider getIncorrectLengthLineProvider
+     */
+    public function testIncorrectLengthLine($line)
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->sut->getScore($line);
+    }
+
+    /**
+     * @test
+     * @dataProvider getLineAllFoursProvider
+     */
+    public function testAllFoursGetScore($line)
+    {
+        $this->assertEquals(80, $this->sut->getScore($line));
+    }
+
+    /**
+     * @test
+     * @dataProvider getLineSparkAndFourProvider
+     */
+    public function testSparkAndFourGetScore($line)
+    {
+        $this->assertEquals(92, $this->sut->getScore($line));
+    }
+
+    /**
+     * @test
+     * @dataProvider getLineSparkAndFourWith21FramesProvider
+     */
+    public function testSparkAndFourWith21FramesGetScore($line)
+    {
+        $this->assertEquals(93, $this->sut->getScore($line));
+    }
+
+    /**
+     * @test
+     * @dataProvider getLineStrikeAndFourProvider
+     */
+    public function testStrikeAndFourGetScore($line)
+    {
+        $this->assertEquals(90, $this->sut->getScore($line));
+    }
+
+    /**
+     * @test
+     * @dataProvider getLineMixedProvider
+     */
+    public function testMixedLineGetScore($line, $expected)
+    {
+        $this->assertEquals($expected, $this->sut->getScore($line));
+    }
+
+    /**
+     * @test
+     * @dataProvider getLineStrikeAndFourWith21FramesProvider
+     */
+    public function testStrikeAndFourWith21FramesGetScore($line)
+    {
+        $this->assertEquals(108, $this->sut->getScore($line));
+    }
+
+    /**
+     * @test
+     * @dataProvider getLineStrikeAndFinalRandom
+     */
+    public function testStrikeAndFinalRandom($line)
+    {
+        $this->assertEquals(280, $this->sut->getScore($line));
+    }
+
+    public function getIncorrectLengthLineProvider()
+    {
+        return [
+            ['0 0 0']
+        ];
+    }
+
+    public function getLineAllFoursProvider()
+    {
+        return [
+            ['4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4']
+        ];
+    }
+
+    public function getLineAllGuttersProvider()
+    {
+        return [
+            ['0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0']
+        ];
+    }
+
+    public function getLineSparkAndFourProvider()
+    {
+        return [
+            ['5 5 4 4 5 5 4 4 4 4 4 4 4 4 4 4 4 4 4 4']
+        ];
+    }
+
+    public function getLineSparkAndFourWith21FramesProvider()
+    {
+        return [
+            ['5 5 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 5 5 5']
+        ];
+    }
+
+    public function getLineStrikeAndFourProvider()
+    {
+        return [
+            ['10 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4']
+        ];
+    }
+
+    public function getLineMixedProvider()
+    {
+        return [
+            ['3 3 10 10 3 3 3 3 3 3 3 3 3 3 3 3 3 3', 87],
+            ['10 10 10 10 10 10 10 10 10 10 10 10', 300],
+            ['9 0 9 0 9 0 9 0 9 0 9 0 9 0 9 0 9 0 9 0',90],
+            ['5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5', 150]
+        ];
+    }
+
+    public function getLineStrikeAndFourWith21FramesProvider()
+    {
+        return [
+            ['5 5 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 10 10 10']
+        ];
+    }
+
+    public function getLineStrikeAndFinalRandom() {
+        return [
+            ['10 10 10 10 10 10 10 10 10 10 3 4']
+        ];
+    }
 }
