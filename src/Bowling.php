de66d5ded888e1c93aa57af82c9c73c1c93119eb
aaronsalva27 <aaronsalva27@gmail.com>
2019-02-08 13:52:13 +0100

    refactor

--- src/Bowling.php
+++ src/Bowling.php
@@ -2,26 +2,54 @@
 
 class Bowling
 {
+    /**
+     * @param $line
+     * @return int
+     */
     public function getScore($line): int
     {
-        $formattedLine = $this->formatLine($line);
-
-        if (count($formattedLine) < 12) {
+        if (count(explode(' ', $line)) < 12) {
             throw new \InvalidArgumentException('Incorrect length');
         }
+
+        $formattedLine = $this->formatLine($line);
 
         return $this->calculateScore($formattedLine);
     }
 
+    /**
+     * @param $line
+     * @return array
+     */
+    private function formatLine($line): array
+    {
+        $formattedLine = [];
 
+        foreach (explode(' ', $line) as $frame) {
+            if (!is_int(intval($frame))) {
+                throw new InvalidArgumentException('Incorrect argument');
+            }
+
+            if (intval($frame) > 10) {
+                throw new InvalidArgumentException('Incorrect argument');
+            }
+
+            array_push($formattedLine, intval($frame));
+        }
+
+        return $formattedLine;
+    }
+
+    /**
+     * @param $line
+     * @return int
+     */
     private function calculateScore($line): int
     {
         $score = 0;
         $framesCount = 0;
 
-        for ($currentFrame = 0; $currentFrame < count($line) - 1;
-             $currentFrame += 2) {
-
+        for ($currentFrame = 0; $currentFrame < count($line) - 1; $currentFrame += 2) {
             $framesCount++;
             $score += $line[$currentFrame];
             $score += $line[$currentFrame + 1];
@@ -43,38 +71,34 @@ class Bowling
         return $score;
     }
 
-    private function isLastFrame($count)
-    {
-        return $count>= 10;
-    }
-
+    /**
+     * @param $line
+     * @param $frameIndex
+     * @return bool
+     */
     private function isStrike($line, $frameIndex): bool
     {
         return $line[$frameIndex] === 10;
     }
 
+    /**
+     * @param $line
+     * @param $frameIndex
+     * @return bool
+     */
     private function isSpare($line, $frameIndex): bool
     {
         return $line[$frameIndex] + $line[$frameIndex + 1] === 10;
     }
 
-    private function formatLine($line): array
+    /**
+     * @param $count
+     * @return bool
+     */
+    private function isLastFrame($count)
     {
-        $formattedLine = [];
-
-        foreach (explode(' ', $line) as $frame) {
-            if (!is_int(intval($frame))) {
-                throw new InvalidArgumentException('Incorrect argument');
-            }
-
-            if (intval($frame) > 10) {
-                throw new InvalidArgumentException('Incorrect argument');
-            }
-
-            array_push($formattedLine, intval($frame));
-        }
-
-        return $formattedLine;
+        return $count >= 10;
     }
+
 
 }
